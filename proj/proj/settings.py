baa7c2650029a3b262aba634908becc6cf64cfd7
dreamcruise <dast670@gmail.com>
2025-01-12 21:32:30 +0300

    added  pages

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['gptru.ru', '176.114.89.155',]
+ALLOWED_HOSTS = ['gptru.ru', '176.114.89.155', '127.0.0.1', ]
 
 AUTH_USER_MODEL = 'accounts.UserAccount'
 LOGIN_REDIRECT_URL = 'home'
